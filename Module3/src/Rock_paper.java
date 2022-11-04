8d223d826edc62684cded72f3120069db1a061c2
Amanda Sherman <111388502+AmandaSherman@users.noreply.github.com>
2022-11-03 19:39:18 -0500

    Finished module 3 rock paper scissors

--- Module3/src/Rock_paper.java
+++ Module3/src/Rock_paper.java
@@ -8,7 +8,28 @@ public class Rock_paper {
         int human = input.nextInt();
 
         int computer = (int)((Math.random() * (4 - 1)) + 1);
-        System.out.println("The computer chose: "+ computer);
+        System.out.print("The computer chose: "+ computer + "-");
+
+        switch (computer) {
+            case 1:
+                System.out.println("Rock");
+                break;
+            case 2:
+                System.out.println("Paper");
+                break;
+            case 3:
+                System.out.println("Scissor");
+        }
+        if (human == computer)
+            System.out.println("Tie Game");
+        else if (((human == 1) && (computer == 2))
+            || ((human == 2) && (computer == 3))
+            || ((human == 3) && (computer == 1)))
+            System.out.println("You lose");
+        else if (((human == 1) && (computer == 3))
+                || ((human == 2) && (computer == 1))
+                || ((human == 3) && (computer == 2)))
+            System.out.println("You win!");
     }
 
 }
