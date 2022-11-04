3c83f7d26532a5c7daf1ee2b0f47bb403dfbc247
Amanda Sherman <111388502+AmandaSherman@users.noreply.github.com>
2022-11-03 19:46:43 -0500

    Finished module 3 rock paper scissors

--- Module3/src/Rock_paper.java
+++ Module3/src/Rock_paper.java
@@ -1,15 +1,25 @@
+/*
+Amanda Sherman
+Bellevue University
+Module 3 Programming Assignment
+
+Rock, Paper, Scissors
+ */
+
 import java.util.Scanner;
 
 public class Rock_paper {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter number 1-rock, 2-paper, or 3-scissor: ");
+        System.out.print("Enter a number 1-Rock, 2-Paper, or 3-Scissor: ");
         int human = input.nextInt();
 
         int computer = (int)((Math.random() * (4 - 1)) + 1);
         System.out.print("The computer chose: "+ computer + "-");
 
+        //I kept getting a prompt to change to an enhanced switch statement.
+        //I kept this one because this is like the version in the text, and it worked fine for me.
         switch (computer) {
             case 1:
                 System.out.println("Rock");
@@ -20,6 +30,8 @@ public class Rock_paper {
             case 3:
                 System.out.println("Scissor");
         }
+
+        //I could have done this if, else if, and else. This made more sense to me though.
         if (human == computer)
             System.out.println("Tie Game");
         else if (((human == 1) && (computer == 2))
