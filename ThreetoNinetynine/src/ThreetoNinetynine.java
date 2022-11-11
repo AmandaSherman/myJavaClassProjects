27ddb789c205b317ba5d2cd5b2bdecfea52b7f4f
Amanda Sherman <111388502+AmandaSherman@users.noreply.github.com>
2022-11-10 21:19:35 -0600

    Module 5 not complete

--- ThreetoNinetynine/src/ThreetoNinetynine.java
+++ ThreetoNinetynine/src/ThreetoNinetynine.java
@@ -7,22 +7,14 @@ Module 5 Assignment
 
 public class ThreetoNinetynine {
     public static void main(String[] args) {
-        //Assign the initial value
-        double num = 3.0;
+        double answer = 0;
+
 //for loop (initial action; condition for loop; action-restart)
-        for (num = 3.0; num <=99.0; num = num +2) {
-            double answer = 1 / num;
-            //loop body
-            System.out.println(answer);
+        for (int num = 3; num <=99; num = num +2) {
+            answer += (1 / num);
         }
-//while loop
-        //condition or end value
-        while (num >= 3.0) {
-            double answer = 1 / num;
-            //loop body
-            System.out.println(answer);
-            //action after loop then restart
-            num = num - 2;
-        }
+            System.out.printf("%.4f\n", answer);
+
+
     }
 }
